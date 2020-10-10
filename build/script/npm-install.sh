c07c49bee9c761ed1e3562773ad4ef5e223cffa5
hylian-flute <hylian_flute@yahoo.co.jp>
2020-10-10 15:17:12 +0900

    npm-installのときにpkgをコピーするようにする

--- build/script/npm-install.sh
+++ build/script/npm-install.sh
@@ -1,3 +1,5 @@
-cp /home/wasm-app/www/package*.json /home/module && \
-cd /home/module && \
+rm -rf /home/module/pkg && \
+cp -r /home/wasm-app/pkg /home/module/pkg && \
+cp /home/wasm-app/www/package*.json /home/module/www && \
+cd /home/module/www && \
 npm install
