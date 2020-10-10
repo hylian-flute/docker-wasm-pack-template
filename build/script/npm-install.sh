4869d1316cc2f47f3812352671608b1320f013aa
hylian-flute <hylian_flute@yahoo.co.jp>
2020-10-10 15:54:54 +0900

    npm-install時のpkgの扱いに関する変更を破棄

--- build/script/npm-install.sh
+++ build/script/npm-install.sh
@@ -1,5 +1,3 @@
-rm -rf /home/module/pkg && \
-cp -r /home/wasm-app/pkg /home/module/pkg && \
-cp /home/wasm-app/www/package*.json /home/module/www && \
-cd /home/module/www && \
+cp /home/wasm-app/www/package*.json /home/module && \
+cd /home/module && \
 npm install
